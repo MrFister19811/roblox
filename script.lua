a889cff2894efbef8066fc69f88b82444b675047
MrFister19811 <106437591+MrFister19811@users.noreply.github.com>
2022-09-19 14:38:01 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 wait(1.2)
 game.StarterGui:SetCore("SendNotification", {
-Title = "DISCORD"; -- the title (ofc)
-Text = "LINK, - Security at it's finest"; -- what the text says (ofc)
-Icon = "rbxassetid://57254792"; -- the image if u want. 
-Duration = 5; -- how long the notification should in secounds
+Title = "Discord";
+Text = "LINK, - Security at it's finest";
+Icon = "rbxassetid://57254792";
+Duration = 5;
 })
